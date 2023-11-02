9ad164eaea60acb6e67715aa46d80f07e151a397
Mizi00 <toufikboukichou.sio@gmail.com>
2023-11-02 13:01:35 +0100

    Fill helper table

--- database/migrations/2023_11_02_103252_create_helpers_table.php
+++ database/migrations/2023_11_02_103252_create_helpers_table.php
@@ -12,8 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('helpers', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->increment('id'); //clé primaire étrangère
+
+            //relation avec la clé etrangère
+            $table->foreign('id')->reference('id')->on('employees');
         });
     }
 
