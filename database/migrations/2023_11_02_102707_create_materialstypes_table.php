a2daf9248a19b8a9b4246f05cff75ea845389c38
Mizi00 <toufikboukichou.sio@gmail.com>
2023-11-02 12:20:15 +0100

    Fill materialstypes table

--- database/migrations/2023_11_02_102707_create_materialstypes_table.php
+++ database/migrations/2023_11_02_102707_create_materialstypes_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('materialstypes', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->increment('id');
+            $table->string('typeLabel');
         });
     }
 
