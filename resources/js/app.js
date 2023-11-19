430bed96c101be0bb0941ae4984b05aa991cf0ad
Shuudy <ancieux.jerome059@gmail.com>
2023-11-19 20:38:38 +0100

    Add scss files

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,26 @@
-import './bootstrap';
+
+import '../css/app.scss';
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Sélectionnez les éléments
+    var auth = document.querySelector('.navbar-auth');
+    var authDropdown = document.querySelector('.navbar-auth-dropdown');
+    var isDropdownOpen = false;
+
+    auth.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        if (!e.target.closest('.navbar-auth-dropdown')) {
+            authDropdown.classList.toggle('show');
+            isDropdownOpen = !isDropdownOpen;
+        }
+    });
+
+    document.addEventListener('click', function (e) {
+        var target = e.target;
+        if (isDropdownOpen && !target.closest('.navbar-auth') && !target.closest('.navbar-auth-dropdown')) {
+            authDropdown.classList.remove('show');
+            isDropdownOpen = false;
+        }
+    });
+});
