2a193ce15036a57ff2011abd826860cd3b827d1e
Shuudy <ancieux.jerome059@gmail.com>
2023-11-20 21:55:11 +0100

    Fix console error with navbar-auth

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,25 +4,27 @@ import '../css/app.scss';
 document.addEventListener('DOMContentLoaded', function () {
     // Sélectionnez les éléments
     var auth = document.querySelector('.navbar-auth');
-    var authDropdown = document.querySelector('.navbar-auth-dropdown');
-    var isDropdownOpen = false;
+    if (auth) {
+        var authDropdown = document.querySelector('.navbar-auth-dropdown');
+        var isDropdownOpen = false;
 
-    auth.addEventListener('click', function (e) {
-        e.preventDefault();
+        auth.addEventListener('click', function (e) {
+            e.preventDefault();
 
-        if (!e.target.closest('.navbar-auth-dropdown')) {
-            authDropdown.classList.toggle('show');
-            isDropdownOpen = !isDropdownOpen;
-        }
-    });
+            if (!e.target.closest('.navbar-auth-dropdown')) {
+                authDropdown.classList.toggle('show');
+                isDropdownOpen = !isDropdownOpen;
+            }
+        });
 
-    document.addEventListener('click', function (e) {
-        var target = e.target;
-        if (isDropdownOpen && !target.closest('.navbar-auth') && !target.closest('.navbar-auth-dropdown')) {
-            authDropdown.classList.remove('show');
-            isDropdownOpen = false;
-        }
-    });
+        document.addEventListener('click', function (e) {
+            var target = e.target;
+            if (isDropdownOpen && !target.closest('.navbar-auth') && !target.closest('.navbar-auth-dropdown')) {
+                authDropdown.classList.remove('show');
+                isDropdownOpen = false;
+            }
+        });
+    }
 });
 
 // CTRL + SHIFT + S, to show sidebar for debug
