718a47f00bc876df5a62a266ecea2dd3a827bb8a
Shuudy <ancieux.jerome059@gmail.com>
2023-11-27 21:14:32 +0100

    Update intervention model for relations

--- app/Models/Intervention.php
+++ app/Models/Intervention.php
@@ -9,4 +9,19 @@ class Intervention extends Model
 {
     use HasFactory;
     public $timestamps = false;
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class, 'clientNum');
+    }
+
+    public function technician()
+    {
+        return $this->belongsTo(Technician::class, 'registrationNum');
+    }
+
+    public function interventionsheet()
+    {
+        return $this->belongsTo(InterventionSheet::class, 'sheetNum');
+    }
 }
