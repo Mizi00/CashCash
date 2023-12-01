77d292d95de88cae6ef33549d7181a8d64ac7ba0
Shuudy <ancieux.jerome059@gmail.com>
2023-12-01 22:22:09 +0100

    Add test for search by technician id

--- tests/Feature/SearchInterventionsTest.php
+++ tests/Feature/SearchInterventionsTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Carbon\Carbon;
 use Tests\TestCase;
 use Livewire\Livewire;
+use App\Models\Technician;
 use App\Models\Intervention;
 use App\Livewire\SearchInterventions;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -25,5 +26,22 @@ class SearchInterventionsTest extends TestCase
         Livewire::test(SearchInterventions::class)
             ->set('searchDate', '2018-06-08')
             ->assertSee($formattedDate);
+    }
+
+    public function test_can_search_interventions_by_technician_id(): void
+    {
+        $technician = Technician::factory()->create();
+
+        $intervention = Intervention::factory()->create([
+            'dateTimeVisit' => '2022-11-24 20:00:00',
+            'registrationNum' => $technician->id
+        ]);
+
+        $formattedDate = Carbon::parse($intervention->dateTimeVisit)->format('d/m/Y H:i');
+
+        Livewire::test(SearchInterventions::class)
+            ->set('searchTech', $technician->id)
+            ->assertSee($formattedDate)
+            ->assertSee($intervention->registrationNum);   
     }
 }
