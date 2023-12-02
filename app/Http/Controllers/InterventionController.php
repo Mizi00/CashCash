3da4fdee6fd82a67103aec34ec24b0cf6312148a
Shuudy <ancieux.jerome059@gmail.com>
2023-12-03 00:16:24 +0100

    Add update method in InterventionController

--- app/Http/Controllers/InterventionController.php
+++ app/Http/Controllers/InterventionController.php
@@ -18,4 +18,14 @@ class InterventionController extends Controller
 
         return view('interventions.edit', compact('intervention'));
     }
+
+    public function update(Request $request, $id)
+    {
+        // Check form fields
+        $credentials = $request->validate([
+
+        ]);
+
+        $intervention = Intervention::findOrFail($id);
+    }
 }
