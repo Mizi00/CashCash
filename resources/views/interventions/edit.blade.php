0b21349fd85c707a37635f6af9c8a8eb787bcdfc
Shuudy <ancieux.jerome059@gmail.com>
2023-12-03 00:16:01 +0100

    Change form fields

--- resources/views/interventions/edit.blade.php
+++ resources/views/interventions/edit.blade.php
@@ -8,27 +8,22 @@
     <main class="main-content">
         <h4 class="main-title">Data tables</h4>
         <div class="main-block">
-            <form action="">
-                <div class="form-row">
-                    <div class="form-group">
-                        <label for="firstname">Firstname</label>
-                        <input type="email" id="firstname" class="form-control" placeholder="Enter firstname">
-                    </div>
-                    <div class="form-group">
-                        <label for="lastname">Lastname</label>
-                        <input type="email" id="lastname" class="form-control" placeholder="Enter lastname">
-                    </div>
-                </div>
-                
+            <form action="{{ route('interventions.update', $intervention->id) }}" method="post">
+                @csrf
                 <div class="form-group">
-                    <label for="address">Address</label>
-                    <input type="text" class="form-control" id="address" placeholder="Address">
+                    <label for="datetimevisit">Date of visit</label>
+                    <input type="datetime-local" name="datetimevisit" id="datetimevisit" class="form-control" value="{{ $intervention->dateTimeVisit }}">
                 </div>
-
-                <div class="form-group">
-                    <label for="comment">Comment</label>
-                    <textarea name="" id="comment" cols="30" class="form-control" rows="10"></textarea>
-                </div>
+                @if($intervention->technician == null)
+                    <fieldset disabled="disabled">
+                @endif
+                    <div class="form-group">
+                        <label for="registrationnum">Registration number</label>
+                        <input type="number" name="registrationnum" id="registrationnum" class="form-control" value="{{ optional($intervention->technician)->id ?? '' }}" placeholder="{{ $intervention->technician === null ? 'No technician assigned' : '' }}">
+                    </div>
+                @if($intervention->technician == null)
+                    </fieldset>
+                @endif
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
         </div>
