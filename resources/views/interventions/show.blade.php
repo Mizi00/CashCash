211155b1cac4d367b6539c8aa2fc59e731afc157
Shuudy <ancieux.jerome059@gmail.com>
2023-12-05 17:21:43 +0100

    Add status with badge for interventions and test delete

--- resources/views/interventions/show.blade.php
+++ resources/views/interventions/show.blade.php
@@ -7,9 +7,13 @@
 <div class="page-container">
     <main class="main-content">
         <h4 class="main-title">Data tables</h4>
-        <div class="main-block">
+        <div class="main-block">            
             <p><strong>Client:</strong> {{ $intervention->client->socialReason }}</p>
-            <p><strong>Technician:</strong> {{ optional($intervention->technician)->employee->lastName ?? 'No technician assigned' }}</p>
+            @php
+                $technician = optional($intervention->technician);
+                $employee = optional($technician->employee);
+            @endphp
+            <p><strong>Technician:</strong> {{ implode(' ', array_filter([$employee->firstName, $employee->lastName])) ?: 'No technician assigned' }}</p>
             <p><strong>Address:</strong> {{ $intervention->client->address }}</p>
             @php
                 $formattedDate = \Carbon\Carbon::parse($intervention->dateTimeVisit)->isoFormat('MMM D, YYYY HH[:]mm A');
--- resources/views/livewire/search-interventions.blade.php
+++ resources/views/livewire/search-interventions.blade.php
@@ -1,5 +1,6 @@
 <div>
-    <div class="table-search">
+    <div class="table-search spaced">
+        <label>Show <select wire:model.live="perPage" name="" id=""><option value="10">10</option><option value="25">25</option><option value="50">50</option></select> entries</label>
         <label>Search:<input type="date" wire:model.live="searchDate" aria-controls="table"><input type="number" placeholder="Registration number" wire:model.live="searchTech" aria-controls="table"></label>
     </div>
     <table>
@@ -8,8 +9,10 @@
                 <th>Visit date</th>
                 <th>Client</th>
                 <th>Technician</th>
+                <th>Status</th>
                 <th>Start date</th>
                 <th>Salary</th>
+                <th></th>
             </tr>
         </thead>                
         <tbody>
@@ -18,12 +21,14 @@
                     <td>{{ Carbon\Carbon::parse($intervention->dateTimeVisit)->isoFormat('MMM D, YYYY HH[:]mm A') }}</td>
                     <td>{{ $intervention->client->socialReason }}</td>
                     <td>{{ optional($intervention->technician)->id ?? 'N/A' }}</td>
+                    <td><span class="badge {{ is_null($intervention->technician) ? 'red' : ($intervention->isCompleted() ? 'green' : 'orange') }}">{{ is_null($intervention->technician) ? 'Unassigned' : ($intervention->isCompleted() ? 'Completed' : 'In progress') }}</span></td>
                     <td>2009/10/09</td>
                     <td>$1,200,000</td>
+                    <td><div class="table-actions"><a href="{{ route('interventions.show', $intervention->id) }}"><i class="fa-regular fa-eye"></i></a><a href="{{ route('interventions.edit', $intervention->id) }}"><i class="fa-regular fa-pen-to-square"></i></a></div></td>
                 </tr>
             @empty
                 <tr>
-                    <td colspan="6">No interventions found.</td>
+                    <td colspan="7">No interventions found.</td>
                 </tr>
             @endforelse
         </tbody>
