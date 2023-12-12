f0848166c65fd5ec7c2f1f6c6af7964cd42f7c31
Mizi00 <toufikboukichou.sio@gmail.com>
2023-12-12 21:58:15 +0100

    Creaton of client test

---
+++ tests/Feature/ClientsTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\Client;
+use App\Models\Helper;
+use Livewire\Livewire;
+use App\Models\Employee;
+use App\Livewire\SearchClients;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ClientsTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function test_can_search_clients_by_id(): void
+    {
+        $client = Client::factory()->create([
+            'id' => 535
+        ]);
+
+        Livewire::test(SearchClients::class)
+            ->set('search', '535')
+            ->assertSee($client->id);
+    }
+
+    public function test_can_edit_client(): void
+    {
+        $client = Client::factory()->create();
+
+        $employee = Employee::factory()->create();
+        $helper = Helper::factory()->create(['id' => $employee->id]);
+
+        $this->actingAs($employee);
+        $response = $this->patch(route('clients.update', $client->id), [
+            'socialReason' => 'Auchan',
+            'sirenNum' => '000000',
+            'apeCode' => '12121DDD',
+            'address' => '23 rue de lafièvre',
+            'phoneNumber' => '0610101033',
+            'faxNum' => '0909902232',
+            'mailAddress' => 'new@email.com',
+        ]);
+
+        $response->assertRedirect();
+
+        dd($client->socialReason);
+        $client->refresh();
+
+        $this->assertEquals('Auchan', $client->socialReason);
+        $this->assertEquals('New SIREN Number', $client->sirenNum);
+        $this->assertEquals('12121DDD', $client->apeCode);
+        $this->assertEquals('23 rue de lafièvre', $client->address);
+        $this->assertEquals('0610101033', $client->phoneNumber);
+        $this->assertEquals('0909902232', $client->faxNum);
+        $this->assertEquals('new@email.com', $client->mailAddress);
+    }
+}
