058dacbf59b8a31166b2860d3bc382f1c18929c0
Shuudy <ancieux.jerome059@gmail.com>
2023-12-18 15:12:09 +0100

    Change route in sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -2,14 +2,36 @@
     <div class="sidebar-logo">CashCash</div>
     <ul class="sidebar-menu">
         <!-- Sidebar menu item -->
-        @for($i=0;$i<5;$i++)
         <li>
-            <a href="">
+            <a href="{{ route('clients.index') }}">
                 <span class="icon"><i class="fa-solid fa-stroopwafel"></i></span>
-                <span class="title">UI Elements</span>
+                <span class="title">Clients</span>
             </a>
         </li>
-        @endfor
+        <li>
+            <a href="{{ route('assignments.index') }}">
+                <span class="icon"><i class="fa-solid fa-stroopwafel"></i></span>
+                <span class="title">Assignments</span>
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('interventions.index') }}">
+                <span class="icon"><i class="fa-solid fa-stroopwafel"></i></span>
+                <span class="title">Iternventions</span>
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('techinterventions.index') }}">
+                <span class="icon"><i class="fa-solid fa-stroopwafel"></i></span>
+                <span class="title">Validations</span>
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('techstats.index') }}">
+                <span class="icon"><i class="fa-solid fa-stroopwafel"></i></span>
+                <span class="title">Statistics</span>
+            </a>
+        </li>
     </ul>
 </div>
 <div class="mask" id="mask"></div>
