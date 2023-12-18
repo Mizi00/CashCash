f8046df4d1614acd50020d212fce7294ee0b8218
Shuudy <ancieux.jerome059@gmail.com>
2023-12-18 15:22:30 +0100

    Add icons to sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -4,31 +4,31 @@
         <!-- Sidebar menu item -->
         <li>
             <a href="{{ route('clients.index') }}">
-                <span class="icon"><i class="fa-solid fa-stroopwafel"></i></span>
+                <span class="icon"><i class="fa-solid fa-user"></i></span>
                 <span class="title">Clients</span>
             </a>
         </li>
         <li>
             <a href="{{ route('assignments.index') }}">
-                <span class="icon"><i class="fa-solid fa-stroopwafel"></i></span>
+                <span class="icon"><i class="fa-solid fa-user-plus"></i></span>
                 <span class="title">Assignments</span>
             </a>
         </li>
         <li>
             <a href="{{ route('interventions.index') }}">
-                <span class="icon"><i class="fa-solid fa-stroopwafel"></i></span>
-                <span class="title">Iternventions</span>
+                <span class="icon"><i class="fa-solid fa-toolbox"></i></span>
+                <span class="title">Interventions</span>
             </a>
         </li>
         <li>
             <a href="{{ route('techinterventions.index') }}">
-                <span class="icon"><i class="fa-solid fa-stroopwafel"></i></span>
+                <span class="icon"><i class="fa-solid fa-circle-check"></i></span>
                 <span class="title">Validations</span>
             </a>
         </li>
         <li>
             <a href="{{ route('techstats.index') }}">
-                <span class="icon"><i class="fa-solid fa-stroopwafel"></i></span>
+                <span class="icon"><i class="fa-solid fa-chart-simple"></i></span>
                 <span class="title">Statistics</span>
             </a>
         </li>
