4feb67fdb80c3cf81ca3450a1b74bedb1a17110f
Shuudy <ancieux.jerome059@gmail.com>
2024-01-06 17:45:48 +0100

    Add sidebar link active when on page for better visibility

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -4,25 +4,25 @@
         <!-- Sidebar menu item -->
         @if(!auth()->user()->isTechnician())
         <li>
-            <a href="{{ route('index') }}">
+            <a href="{{ route('index') }}" class="{{ Route::is('index') ? 'active' : '' }}">
                 <span class="icon"><i class="fa-solid fa-house-chimney"></i></span>
                 <span class="title">Dashboard</span>
             </a>
         </li>
         <li>
-            <a href="{{ route('clients.index') }}">
+            <a href="{{ route('clients.index') }}" class="{{ Route::is('clients.*') ? 'active' : '' }}">
                 <span class="icon"><i class="fa-solid fa-user"></i></span>
                 <span class="title">Clients</span>
             </a>
         </li>
         <li>
-            <a href="{{ route('assignments.index') }}">
+            <a href="{{ route('assignments.index') }}" class="{{ Route::is('assignments.*') ? 'active' : '' }}">
                 <span class="icon"><i class="fa-solid fa-user-plus"></i></span>
                 <span class="title">Assignments</span>
             </a>
         </li>
         <li>
-            <a href="{{ route('interventions.index') }}">
+            <a href="{{ route('interventions.index') }}" class="{{ Route::is('interventions.*') ? 'active' : '' }}">
                 <span class="icon"><i class="fa-solid fa-toolbox"></i></span>
                 <span class="title">Interventions</span>
             </a>
@@ -30,7 +30,7 @@
         @endif
         @if(auth()->user()->isTechnician())
             <li>
-                <a href="{{ route('techinterventions.index') }}">
+                <a href="{{ route('techinterventions.index') }}" class="{{ Route::is('techinterventions.*') ? 'active' : '' }}">
                     <span class="icon"><i class="fa-solid fa-circle-check"></i></span>
                     <span class="title">Validations</span>
                 </a>
@@ -38,7 +38,7 @@
         @endif
         @if(!auth()->user()->isTechnician()) 
         <li>
-            <a href="{{ route('techstats.index') }}">
+            <a href="{{ route('techstats.index') }}" class="{{ Route::is('techstats.*') ? 'active' : '' }}">
                 <span class="icon"><i class="fa-solid fa-chart-simple"></i></span>
                 <span class="title">Statistics</span>
             </a>
