ad3e26d228f77efb2396174eec5f82c407dfed5d
Shuudy <ancieux.jerome059@gmail.com>
2024-05-02 21:52:01 +0200

    Add Family model namespace

--- database/seeders/FamiliesTableSeeder.php
+++ database/seeders/FamiliesTableSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Family;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class FamiliesTableSeeder extends Seeder
 {
